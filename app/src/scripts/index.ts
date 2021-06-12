ce2deed449113b9714c60e264e6026c31b200149
Shahriyor <shahriyordeveloping@gmail.com>
2021-06-13 02:11:27 +0300

    initial

---
+++ app/src/scripts/index.ts
@@ -0,0 +1,23 @@
+new WOW().init();
+// Header Menu
+const headerButton: HTMLButtonElement = document.querySelector(
+  ".header__button"
+);
+const headerMenu: HTMLUListElement = document.querySelector(".header__nav");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+
+headerButton.onclick = menuToggle;
+
+window.onclick = (e: MouseEvent) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
